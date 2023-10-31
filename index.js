887fefa3f086c49219548388d34f895a581fb260
diazarm <marceloardiaz@gmail.com>
2023-10-31 13:22:33 -0300

    fix router

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require ('cors');
-const mainRouter = require('./src/routes/mainRouter');
+const clientRoute = require('./src/routes/clientRoute');
 
 const PORT = 3001;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(mainRouter);
+
+app.use("/", clientRoute);
 
 app.listen(PORT, ()=>{
     console.log(`Server active on port ${PORT}`);
