095f479b420562570aec4cd17458b8452004842e
diazarm <marceloardiaz@gmail.com>
2023-11-03 10:38:53 -0300

    cleaning to record

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const cors = require ('cors');
-const clientRoute = require('./src/routes/clientRoute');
 
 const PORT = 3001;
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
-app.use("/", clientRoute);
+
 
 app.listen(PORT, ()=>{
     console.log(`Server active on port ${PORT}`);
