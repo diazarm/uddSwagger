b83d98dba19961ac5b70e5b6a53c1df406ee692e
diazarm <marceloardiaz@gmail.com>
2023-11-05 11:31:49 -0300

    update

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res)=>{
+    res.status(200).json({msg : "Estas en la Home"})
+} )
 
+app.get("/mensaje", (req, res)=>{
+    const {name, surname} = req.query;
+    res.json({msg: `saludos a ${name} ${surname}`})
+})
+
+//ttp://localhost:3001/mensaje/?name=Victor&surname=Perez
 
 app.listen(PORT, ()=>{
     console.log(`Server active on port ${PORT}`);
